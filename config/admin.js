15c68959967d341f93d4307757303ef7d5e7052e
socialmedia-ux <socialmedia@taxiat.com.sa>
2025-08-17 14:07:45 +0300

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -9,9 +9,4 @@ module.exports = ({ env }) => ({
     token: {
       salt: env('tL3pQ8+7D0kZH6zOc6JxfQ=='),
     },
-  },
-  flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
-  },
 });
