8d07a78570c8faa5734cadb7718dbc7fe90c8fcd
socialmedia-ux <socialmedia@taxiat.com.sa>
2025-08-17 14:25:30 +0300

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   'users-permissions': {
     config: {
-      jwtSecret: env('JWT_SECRET', 'iLJqdmkuu/E2uaynVrYyKg=='),
+      jwtSecret: env('JWT_SECRET', '1fd8c1217e3121963b0706b473e67d0b'),
     },
   },
 });
