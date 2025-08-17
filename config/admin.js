2a2e6ac8d17d8544230701d15e28d8ea8c03a4e2
socialmedia-ux <socialmedia@taxiat.com.sa>
2025-08-17 14:04:27 +0300

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('1fd8c1217e3121963b0706b473e67d0b'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('02aQqVIif8QV5JVz0NDCCQ=='),
   },
   transfer: {
     token: {
